f629abec4571a013326a12df3aa8fe7d44d5e442
Solen <160152102+Naxxale@users.noreply.github.com>
2024-07-16 10:51:27 +0200

    initialisation projet + créa branche Solen

---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container";
+
+
+
+function Footer(){
+    return(
+        <>
+        <Container>
+            <span>Réseau social 1</span>
+            <span>Réseau social 2</span>
+            <span>Réseau social 3</span>
+        </Container>
+        </>
+    )
+
+}
+export default Footer;
